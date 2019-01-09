58efe2c79745209b1a777104f40aac35214444dd
Ales Varabyou <alevar@gmail.com>
2019-01-08 20:03:58 -0500

    uncommented stages

--- runChimFinderOnAll.sh
+++ runChimFinderOnAll.sh
@@ -17,26 +17,26 @@ for file in ${inputDir}/*/*R1*fastq.gz ; do
     echo ${dirP} 
 
     echo ANALYZING ${outputDir}_1/tmp/alns/${sample}.hiv.hxb2.bowtie.sam
-    # ./chimFinder.py --input1r1 ${outputDir}_1/tmp/alns/${sample}.hiv.bowtie.sam \
-                    # --input2r1 ${outputDir}_1/tmp/alns/${sample}.hum.bowtie.sam \
-                    # --input1r2 ${outputDir}_2/tmp/alns/${sample}.hiv.bowtie.sam \
-                    # --input2r2 ${outputDir}_2/tmp/alns/${sample}.hum.bowtie.sam \
-                    # --splicedR1 ${outputDir}_1/tmp/alns/${sample}.hum.hisat.sam \
-                    # --splicedR2 ${outputDir}_2/tmp/alns/${sample}.hum.hisat.sam \
-                    # -o ${outputDir}/${sample}_2550_mc1 \
-                    # -t 12 \
-                    # --minLen 30 \
-                    # --maxLenUnmapped 5 \
-                    # --minCount 1 \
-                    # --maxCountPenalty 1 \
-                    # -a ${annotation} \
-                    # --overlap 5 \
-                    # --gap 5 \
-                    # --minEntropy 0.84 \
-                    # --close 30 \
-                    # --score 0.8 \
-                    # -q
-    # samtools mpileup -uf /ccb/salz3/avaraby/genomes/HIV1/HXB2/HXB2.fa ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam | bcftools call -c --ploidy 1 | vcfutils.pl vcf2fq | seqtk seq -a - > ${outputDir}/consensus/${sample}.hiv.hxb2.SEP.fa
+    ./chimFinder.py --input1r1 ${outputDir}_1/tmp/alns/${sample}.hiv.bowtie.sam \
+                    --input2r1 ${outputDir}_1/tmp/alns/${sample}.hum.bowtie.sam \
+                    --input1r2 ${outputDir}_2/tmp/alns/${sample}.hiv.bowtie.sam \
+                    --input2r2 ${outputDir}_2/tmp/alns/${sample}.hum.bowtie.sam \
+                    --splicedR1 ${outputDir}_1/tmp/alns/${sample}.hum.hisat.sam \
+                    --splicedR2 ${outputDir}_2/tmp/alns/${sample}.hum.hisat.sam \
+                    -o ${outputDir}/${sample}_2550_mc1 \
+                    -t 12 \
+                    --minLen 30 \
+                    --maxLenUnmapped 5 \
+                    --minCount 1 \
+                    --maxCountPenalty 1 \
+                    -a ${annotation} \
+                    --overlap 5 \
+                    --gap 5 \
+                    --minEntropy 0.84 \
+                    --close 30 \
+                    --score 0.8 \
+                    -q
+    samtools mpileup -uf /ccb/salz3/avaraby/genomes/HIV1/HXB2/HXB2.fa ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam | bcftools call -c --ploidy 1 | vcfutils.pl vcf2fq | seqtk seq -a - > ${outputDir}/consensus/${sample}.hiv.hxb2.SEP.fa
     samtools view -h ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam > ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.sam
     sam2splice.py -i ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.sam -o ${outputDir}/${sample}.sj.csv --fasta
     sam2splice.py -i ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.sam -o ${outputDir}/${sample}.sj.f.csv -f
--- wrapper.sh
+++ wrapper.sh
@@ -198,101 +198,101 @@ for file in ${inputDir}/*R1*fastq.gz ; do
 
 		#LANDSCAPES=============================================LANDSCAPES
 
-		# SECONDS=0
-		# echo HISAT ALIGNING R1 TO HXB2
-		# hisat2 -p ${threads} --very-sensitive --end-to-end --novel-splicesite-outfile ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.novelSites --known-splicesite-infile ${hivSplicing} --no-unal --phred33 -x ${HXB2} -U ${outputDir}_R1/tmp/fq/${sample}${baseEnd}_R1.fastq -S ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.sam --un ${outputDir}_R1/tmp/fq/${sample}${baseEnd}.un.fastq
-		# DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
-		# echo DONE IN ${DUR}
-		# TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
+		SECONDS=0
+		echo HISAT ALIGNING R1 TO HXB2
+		hisat2 -p ${threads} --very-sensitive --end-to-end --novel-splicesite-outfile ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.novelSites --known-splicesite-infile ${hivSplicing} --no-unal --phred33 -x ${HXB2} -U ${outputDir}_R1/tmp/fq/${sample}${baseEnd}_R1.fastq -S ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.sam --un ${outputDir}_R1/tmp/fq/${sample}${baseEnd}.un.fastq
+		DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
+		echo DONE IN ${DUR}
+		TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
 
-		# SECONDS=0
-		# echo BOWTIE2 ALIGNING R1 TO HXB2
-		# bowtie2 --very-sensitive-local --no-unal --local --phred33 -p ${threads} -x ${HXB2} -U ${outputDir}_R1/tmp/fq/${sample}${baseEnd}.un.fastq -S ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.sam
-		# rm ${outputDir}_R1/tmp/fq/${sample}${baseEnd}.un.fastq
-		# DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
-		# echo DONE IN ${DUR}
-		# TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
+		SECONDS=0
+		echo BOWTIE2 ALIGNING R1 TO HXB2
+		bowtie2 --very-sensitive-local --no-unal --local --phred33 -p ${threads} -x ${HXB2} -U ${outputDir}_R1/tmp/fq/${sample}${baseEnd}.un.fastq -S ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.sam
+		rm ${outputDir}_R1/tmp/fq/${sample}${baseEnd}.un.fastq
+		DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
+		echo DONE IN ${DUR}
+		TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
 
-		# SECONDS=0
-		# echo INDEXING R1
-		# samtools view -S -@ ${threads} -b ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hum.bowtie.sam | samtools sort -o ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hum.bowtie.bam -
-		# samtools index -@ ${threads} ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hum.bowtie.bam
+		SECONDS=0
+		echo INDEXING R1
+		samtools view -S -@ ${threads} -b ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hum.bowtie.sam | samtools sort -o ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hum.bowtie.bam -
+		samtools index -@ ${threads} ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hum.bowtie.bam
 
-		# samtools view -S -@ ${threads} -b ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.bowtie.sam | samtools sort -o ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.bowtie.bam -
-		# samtools index -@ ${threads} ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.bowtie.bam
+		samtools view -S -@ ${threads} -b ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.bowtie.sam | samtools sort -o ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.bowtie.bam -
+		samtools index -@ ${threads} ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.bowtie.bam
 
-		# samtools view -S -@ ${threads} -b ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hum.hisat.sam | samtools sort -o ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hum.hisat.bam -
-		# samtools index -@ ${threads} ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hum.hisat.bam
+		samtools view -S -@ ${threads} -b ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hum.hisat.sam | samtools sort -o ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hum.hisat.bam -
+		samtools index -@ ${threads} ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hum.hisat.bam
 
-		# samtools view -S -@ ${threads} -b ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.sam | samtools sort -o ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.bam -
-		# samtools index -@ ${threads} ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.bam
+		samtools view -S -@ ${threads} -b ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.sam | samtools sort -o ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.bam -
+		samtools index -@ ${threads} ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.bam
 
-		# samtools view -S -@ ${threads} -b ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.sam | samtools sort -o ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.bam -
-		# samtools index -@ ${threads} ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.bam
-		# DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
-		# echo DONE IN ${DUR}
-		# TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
+		samtools view -S -@ ${threads} -b ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.sam | samtools sort -o ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.bam -
+		samtools index -@ ${threads} ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.bam
+		DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
+		echo DONE IN ${DUR}
+		TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
 
-		# SECONDS=0
-		# echo HISAT ALIGNING R2 TO HXB2
-		# hisat2 -p ${threads} --very-sensitive --end-to-end --novel-splicesite-outfile ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.novelSites --known-splicesite-infile ${hivSplicing} --no-unal --phred33 -x ${HXB2} -U ${outputDir}_R2/tmp/fq/${sample}${baseEnd}_R2.fastq -S ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.sam --un ${outputDir}_R2/tmp/fq/${sample}${baseEnd}.un.fastq
-		# DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
-		# echo DONE IN ${DUR}
-		# TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
+		SECONDS=0
+		echo HISAT ALIGNING R2 TO HXB2
+		hisat2 -p ${threads} --very-sensitive --end-to-end --novel-splicesite-outfile ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.novelSites --known-splicesite-infile ${hivSplicing} --no-unal --phred33 -x ${HXB2} -U ${outputDir}_R2/tmp/fq/${sample}${baseEnd}_R2.fastq -S ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.sam --un ${outputDir}_R2/tmp/fq/${sample}${baseEnd}.un.fastq
+		DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
+		echo DONE IN ${DUR}
+		TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
 
-		# SECONDS=0
-		# echo BOWTIE2 ALIGNING R2 TO HXB2
-		# bowtie2 --very-sensitive-local --no-unal --local --phred33 -p ${threads} -x ${HXB2} -U ${outputDir}_R2/tmp/fq/${sample}${baseEnd}.un.fastq -S ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.sam
-		# rm ${outputDir}_R2/tmp/fq/${sample}${baseEnd}.un.fastq
-		# DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
-		# echo DONE IN ${DUR}
-		# TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
+		SECONDS=0
+		echo BOWTIE2 ALIGNING R2 TO HXB2
+		bowtie2 --very-sensitive-local --no-unal --local --phred33 -p ${threads} -x ${HXB2} -U ${outputDir}_R2/tmp/fq/${sample}${baseEnd}.un.fastq -S ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.sam
+		rm ${outputDir}_R2/tmp/fq/${sample}${baseEnd}.un.fastq
+		DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
+		echo DONE IN ${DUR}
+		TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
 
-		# SECONDS=0
-		# echo INDEXING R2
-		# samtools view -S -@ ${threads} -b ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.bowtie.sam | samtools sort -o ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.bowtie.bam -
-		# samtools index -@ ${threads} ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.bowtie.bam
+		SECONDS=0
+		echo INDEXING R2
+		samtools view -S -@ ${threads} -b ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.bowtie.sam | samtools sort -o ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.bowtie.bam -
+		samtools index -@ ${threads} ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.bowtie.bam
 
-		# samtools view -S -@ ${threads} -b ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.bowtie.sam | samtools sort -o ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.bowtie.bam -
-		# samtools index -@ ${threads} ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.bowtie.bam
+		samtools view -S -@ ${threads} -b ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.bowtie.sam | samtools sort -o ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.bowtie.bam -
+		samtools index -@ ${threads} ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.bowtie.bam
 
-		# samtools view -S -@ ${threads} -b ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.hisat.sam | samtools sort -o ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.hisat.bam -
-		# samtools index -@ ${threads} ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.hisat.bam
+		samtools view -S -@ ${threads} -b ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.hisat.sam | samtools sort -o ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.hisat.bam -
+		samtools index -@ ${threads} ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.hisat.bam
 
-		# samtools view -S -@ ${threads} -b ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.sam | samtools sort -o ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.bam -
-		# samtools index -@ ${threads} ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.bam
+		samtools view -S -@ ${threads} -b ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.sam | samtools sort -o ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.bam -
+		samtools index -@ ${threads} ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.bam
 
-		# samtools view -S -@ ${threads} -b ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.sam | samtools sort -o ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.bam -
-		# samtools index -@ ${threads} ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.bam
-		# DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
-		# echo DONE IN ${DUR}
-		# TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
+		samtools view -S -@ ${threads} -b ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.sam | samtools sort -o ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.bam -
+		samtools index -@ ${threads} ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.bam
+		DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
+		echo DONE IN ${DUR}
+		TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
 
-		# SECONDS=0
-		# echo ANALYZING R2
-		# ./chimFinder.py -i1 ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.bowtie.sam -i2 ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.bowtie.sam --spliced ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.hisat.sam -o ${outputDir}_R2/${sample}${baseEnd} -t 12 --minLen 20 -a ${annotation} --overlap 5 --gap 5 --minEntropy 0.84 --close 5 --score 0.75 -q #> ${outputDir}_R2/${sample}${baseEnd}.report
-		# DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
-		# echo DONE IN ${DUR}
-		# TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
-		# SECONDS=0
+		SECONDS=0
+		echo ANALYZING R2
+		./chimFinder.py -i1 ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.bowtie.sam -i2 ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.bowtie.sam --spliced ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hum.hisat.sam -o ${outputDir}_R2/${sample}${baseEnd} -t 12 --minLen 20 -a ${annotation} --overlap 5 --gap 5 --minEntropy 0.84 --close 5 --score 0.75 -q #> ${outputDir}_R2/${sample}${baseEnd}.report
+		DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
+		echo DONE IN ${DUR}
+		TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
+		SECONDS=0
 
-		# SECONDS=0
-		# echo MERGING SEP
-		# samtools merge -f ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.bam ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.bam ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.bam ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.bam
-		# samtools sort -o ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.sorted.bam ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam
-		# mv ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.sorted.bam ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam
-		# sleep 10s
-		# samtools index ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam.bai
-		# DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
-		# echo DONE IN ${DUR}
-		# TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
+		SECONDS=0
+		echo MERGING SEP
+		samtools merge -f ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.bam ${outputDir}_R1/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.bam ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.bowtie.bam ${outputDir}_R2/tmp/alns/${sample}${baseEnd}.hiv.hxb2.hisat.bam
+		samtools sort -o ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.sorted.bam ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam
+		mv ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.sorted.bam ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam
+		sleep 10s
+		samtools index ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam.bai
+		DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
+		echo DONE IN ${DUR}
+		TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
 
-		# SECONDS=0
-		# echo BUILDING CONSENSUS SEQUENCE FROM SEP
-		# samtools mpileup -uf ${HXB2fa} ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam | bcftools call -c --ploidy 1 | vcfutils.pl vcf2fq | seqtk seq -a - > ${outputDir}/consensus/${sample}${baseEnd}.hiv.hxb2.SEP.fa
-		# DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
-		# echo DONE IN ${DUR}
-		# TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
+		SECONDS=0
+		echo BUILDING CONSENSUS SEQUENCE FROM SEP
+		samtools mpileup -uf ${HXB2fa} ${outputDir}/alns/${sample}${baseEnd}.hiv.hxb2.SEP.bam | bcftools call -c --ploidy 1 | vcfutils.pl vcf2fq | seqtk seq -a - > ${outputDir}/consensus/${sample}${baseEnd}.hiv.hxb2.SEP.fa
+		DUR="$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
+		echo DONE IN ${DUR}
+		TOTAL_TIME=$((TOTAL_TIME + ${SECONDS}))
 
 		T_DUR="$(($TOTAL_TIME / 60)) minutes and $(($TOTAL_TIME % 60)) seconds"
 		echo TOTAL TIME ELAPSED: ${T_DUR}
